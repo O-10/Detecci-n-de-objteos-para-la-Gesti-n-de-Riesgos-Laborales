135e5dc377c2813317e9796097c537e5dc77452c
Oscar Iván Solarte Cerón <149115895+O-10@users.noreply.github.com>
2025-04-18 15:17:00 -0500

    Update streamlit_app.py
    import streamlit as st
    import cv2
    import pandas as pd
    import os
    import tempfile
    import random
    from datetime import datetime, timedelta
    from roboflow import Roboflow
    import matplotlib.pyplot as plt
    from sklearn.feature_extraction.text import TfidfVectorizer
    from sklearn.metrics.pairwise import cosine_similarity

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 #Diseño de sistema de vision computarizada que permite la detección automatica de objetos y corelación semantica del riesgo en entornos laborales como elementos de protección personal, maquinaría, comportamientos inseguros.
 import streamlit as st
 import cv2
